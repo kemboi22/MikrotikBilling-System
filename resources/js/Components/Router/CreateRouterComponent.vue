6960661197b342387c09625689f502776d951a0f
kemboi22 <kemboielvis22@gmail.com>
2024-05-21 06:46:09 +0300

    Added Router View

---
+++ resources/js/Components/Router/CreateRouterComponent.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import {Button} from '@/components/ui/button'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@/components/ui/dialog'
+import {Input} from '@/components/ui/input'
+import {Label} from '@/components/ui/label'
+import {ref} from "vue";
+import {Router} from "@/types";
+
+const form = ref<Router>(<Router>{})
+</script>
+
+<template>
+    <Dialog>
+        <DialogTrigger as-child>
+            <Button>
+                Add Router
+            </Button>
+        </DialogTrigger>
+        <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>Add Router</DialogTitle>
+                <DialogDescription>
+                    Add router and Connect To It
+                </DialogDescription>
+            </DialogHeader>
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-full">
+                    <Label>Router Name</Label>
+                    <Input type="text" v-model="form.name" class="" placeholder="Home Router" autocomplete/>
+                </div>
+                <div class="lg:col-span-6 col-span-full">
+                    <Label>IP Address</Label>
+                    <Input type="text" v-model="form.ipAddress" class="" placeholder="192.168.11.1" autocomplete autofocus/>
+                </div>
+                <div class="lg:col-span-6 col-span-full">
+                    <Label>Port</Label>
+                    <Input type="number" v-model="form.port" class="" placeholder="8728" autocomplete autofocus/>
+                </div>
+                <div class="lg:col-span-6  col-span-full">
+                    <Label>Username</Label>
+                    <Input type="text" v-model="form.username" class="" placeholder="admin" autocomplete/>
+                </div>
+                <div class="lg:col-span-6  col-span-full">
+                    <Label>Password</Label>
+                    <Input type="password" v-model="form.password" class="" placeholder="*********" autocomplete/>
+                </div>
+            </div>
+            <DialogFooter>
+                <Button type="submit">
+                    Save
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
+</template>
--- resources/js/Layouts/DashboardLayout.vue
+++ resources/js/Layouts/DashboardLayout.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-import {Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users} from 'lucide-vue-next'
+import {
+    Bell,
+    CircleUser,
+    Home,
+    LineChart,
+    Menu,
+    Package,
+    Package2,
+    Router,
+    Search,
+    ShoppingCart,
+    Users
+} from 'lucide-vue-next'
 
 import {Badge} from '@/components/ui/badge'
 import {Button} from '@/components/ui/button'
@@ -14,6 +26,14 @@ import {
 } from '@/components/ui/dropdown-menu'
 import {Input} from '@/components/ui/input'
 import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
+import {Link} from "@inertiajs/vue3";
+
+const routes = [
+    { name: 'Routers', icon: Router, href: 'routers.index' },
+]
+const currentRoute = (url: string) => {
+    return route().current(url)
+}
 </script>
 
 <template>
@@ -23,7 +43,7 @@ import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
                 <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                     <a href="/" class="flex items-center gap-2 font-semibold">
                         <Package2 class="h-6 w-6"/>
-                        <span class="">Acme Inc</span>
+                        <span class="">Wifi Internet</span>
                     </a>
                     <Button variant="outline" size="icon" class="ml-auto h-8 w-8">
                         <Bell class="h-4 w-4"/>
@@ -32,61 +52,15 @@ import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
                 </div>
                 <div class="flex-1">
                     <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
-                        <a
-                            href="/"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                        <Link
+                            v-for="rt in routes" :key="rt.href"
+                            :href="route(rt.href)"
+                            :class="['flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary', currentRoute(rt.href) ? 'bg-muted text-primary' : 'text-muted-foreground']"
                         >
-                            <Home class="h-4 w-4"/>
-                            Dashboard
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <ShoppingCart class="h-4 w-4"/>
-                            Orders
-                            <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                6
-                            </Badge>
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-                        >
-                            <Package class="h-4 w-4"/>
-                            Products
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <Users class="h-4 w-4"/>
-                            Customers
-                        </a>
-                        <a
-                            href="#"
-                            class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-                        >
-                            <LineChart class="h-4 w-4"/>
-                            Analytics
-                        </a>
+                            <component :is="rt.icon" class="h-4 w-4"/>
+                            {{ rt.name }}
+                        </Link>
                     </nav>
-                </div>
-                <div class="mt-auto p-4">
-                    <Card>
-                        <CardHeader class="p-2 pt-0 md:p-4">
-                            <CardTitle>Upgrade to Pro</CardTitle>
-                            <CardDescription>
-                                Unlock all features and get unlimited access to our support
-                                team.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent class="p-2 pt-0 md:p-4 md:pt-0">
-                            <Button size="sm" class="w-full">
-                                Upgrade
-                            </Button>
-                        </CardContent>
-                    </Card>
                 </div>
             </div>
         </div>
@@ -112,61 +86,15 @@ import {Sheet, SheetContent, SheetTrigger} from '@/components/ui/sheet'
                                 <Package2 class="h-6 w-6"/>
                                 <span class="sr-only">Acme Inc</span>
                             </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                            <Link
+                                v-for="rt in routes" :key="rt.href"
+                                :href="route(rt.href)"
+                                :class="['flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary', currentRoute(rt.href) ? 'bg-muted text-primary' : 'text-muted-foreground']"
                             >
-                                <Home class="h-5 w-5"/>
-                                Dashboard
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                            >
-                                <ShoppingCart class="h-5 w-5"/>
-                                Orders
-                                <Badge class="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                                    6
-                                </Badge>
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Package class="h-5 w-5"/>
-                                Products
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <Users class="h-5 w-5"/>
-                                Customers
-                            </a>
-                            <a
-                                href="#"
-                                class="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-                            >
-                                <LineChart class="h-5 w-5"/>
-                                Analytics
-                            </a>
+                                <component :is="rt.icon" class="h-4 w-4"/>
+                                {{ rt.name }}
+                            </Link>
                         </nav>
-                        <div class="mt-auto">
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>Upgrade to Pro</CardTitle>
-                                    <CardDescription>
-                                        Unlock all features and get unlimited access to our
-                                        support team.
-                                    </CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <Button size="sm" class="w-full">
-                                        Upgrade
-                                    </Button>
-                                </CardContent>
-                            </Card>
-                        </div>
                     </SheetContent>
                 </Sheet>
                 <div class="w-full flex-1">
