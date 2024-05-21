6963ee1e80eac764a8a9d4908da1ce38bf7df26c
kemboi22 <kemboielvis22@gmail.com>
2024-05-21 08:34:42 +0300

    Added Success and Error Responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,13 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function success(string $message, int $status = 200): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(["success" => true, "message" => $message], 200);
+    }
+
+    public function error(string $message, int $status = 400): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(["success" => false, "message" => $message], $status);
+    }
 }
