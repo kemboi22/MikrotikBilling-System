c7560eacf8762236cc66b21fd36103db397a448d
kemboi22 <kemboielvis22@gmail.com>
2024-05-21 08:35:28 +0300

    CRUD of Router Done

--- app/Http/Controllers/RouterController.php
+++ app/Http/Controllers/RouterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\RouterOS;
 use App\Http\Resources\Router\RouterCollection;
+use App\Http\Resources\Router\RouterResource;
 use App\Models\Router;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -31,16 +33,26 @@ class RouterController extends Controller
             "password" => "required",
             "port" => "required",
         ]);
-        $router = Router::query()->create([
-            "name" => $request->name,
-            "ip_address" => $request->ipAddress,
-            "username" => $request->username,
-            "password" => $request->password,
-            "connect" => true,
-            "port" => $request->port,
-        ]);
-        if ($router) return response()->json(["success" => true, "message" => "Router created successfully"], 201);
-        return response()->json(["success" => false, "message" => "Failed to create router"], 400);
+        try {
+            // Connect To Router
+            $routerAPI = new RouterOS();
+            $connection = $routerAPI->connect($request->ipAddress, $request->username, $request->password);
+            if (!$connection) return response()->json(["success" => false, "message" => "Failed to connect to router"], 400);
+            // Store Router Details
+            $router = Router::query()->create([
+                "name" => $request->name ,
+                "ip_address" => $request->ipAddress,
+                "username" => $request->username,
+                "password" => $request->password,
+                "connect" => $connection,
+                "port" => $request->port,
+            ]);
+            if ($router) return response()->json(["success" => true, "message" => "Router created successfully"], 201);
+            return response()->json(["success" => false, "message" => "Failed to create router"], 400);
+        }catch (\Exception $e) {
+            return response()->json(["success" => false, "message" => $e->getMessage()], 400);
+        }
+
     }
 
     /**
@@ -48,7 +60,7 @@ class RouterController extends Controller
      */
     public function show(Router $router)
     {
-        //
+        return new RouterResource($router);
     }
 
     /**
@@ -63,7 +75,7 @@ class RouterController extends Controller
             "password" => "required",
             "port" => "required",
         ]);
-        $router->update([
+        $update = $router->update([
             "name" => $request->name,
             "ip_address" => $request->ipAddress,
             "username" => $request->username,
@@ -71,7 +83,7 @@ class RouterController extends Controller
             "connect" => true,
             "port" => $request->port,
         ]);
-        if ($router) return response()->json(["success" => true, "message" => "Router updated successfully"], 200);
+        if ($update) return response()->json(["success" => true, "message" => "Router updated successfully"], 200);
         return response()->json(["success" => false, "message" => "Failed to update router"], 400);
     }
 
@@ -80,6 +92,7 @@ class RouterController extends Controller
      */
     public function destroy(Router $router)
     {
-        //
+        if ($router->delete()) return $this->success("Router deleted successfully");
+        return $this->error("Failed to delete router");
     }
 }
